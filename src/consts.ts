74adfb60d898a235f93ff977a36e89dba2bf175d
Oğuzhan Kuşca <oguzhankusca@outlook.com>
2025-05-23 15:47:39 +0300

    Mail adresi düzeltildi.

--- src/consts.ts
+++ src/consts.ts
@@ -3,7 +3,7 @@ import type { Metadata, Site, Socials } from "@types";
 export const SITE: Site = {
   TITLE: "kusca.dev",
   DESCRIPTION: "Frontend Web Developer",
-  EMAIL: "oguzhan@kusca.dev",
+  EMAIL: "oguzhankusca@outlook.com",
   NUM_POSTS_ON_HOMEPAGE: 5,
   NUM_PROJECTS_ON_HOMEPAGE: 3,
 };
